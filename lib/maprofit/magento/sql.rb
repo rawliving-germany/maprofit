7a78ec78c6689dcfecb54d1524da46213d6db5fb
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2018-06-04 06:51:12 +0200

    use mysql reconnecting client and prepared statements

--- lib/maprofit/magento/sql.rb
+++ lib/maprofit/magento/sql.rb
@@ -14,17 +14,38 @@ module Maprofit::Magento
                          port:     conf.dbport,
                          database: conf.dbname,
                          username: conf.dbuser,
-                         password: conf.dbpass)
+                         password: conf.dbpass,
+                         reconnect: true)
+    end
 
+    def self.sales_order order_id
+      query = client.prepare(
+        "
+        SELECT * FROM sales_order
+        WHERE entity_id = ?
+        "
+      )
+      query.execute order_id
+    end
+
+    def self.sales_order_items_for order_id
+      query.prepare(
+        "
+         SELECT * FROM sales_order_item
+         WHERE order_id = ?
+        "
+      )
+      query.execute order_id
     end
 
     def self.sales_invoice invoice_nr
-      client.query(
+      query = client.prepare(
         "
          SELECT * FROM sales_invoice
-         WHERE increment_id = '#{invoice_nr}'
+         WHERE increment_id = '?'
         "
       )
+      query.execute invoice_nr
     end
 
     def self.sales_invoice_between start_date, end_date
@@ -36,25 +57,38 @@ module Maprofit::Magento
       )
     end
 
-    def self.sales_invoice_items_for invoice_order_id
+    def self.sales_order_between start_date, end_date
       client.query(
         "
-         SELECT * FROM sales_invoice_item
-         WHERE order_item_id IN (SELECT item_id FROM sales_order_item
-         WHERE order_id = #{invoice_order_id})
+         SELECT * FROM sales_order
+         WHERE created_at BETWEEN '#{start_date}' AND '#{end_date}'
+         AND status != 'closed' AND status != 'cancelled'
         "
       )
     end
 
-    # varchar attribute for product
-    def self.eav_attr_for product_id, attr_id
-      client.query(
+    def self.sales_invoice_items_for invoice_order_id
+      query = client.prepare(
         "
-         SELECT * FROM catalog_product_entity_varchar
-         WHERE attribute_id = #{attr_id}
-         AND entity_id = #{product_id}
+         SELECT * FROM sales_invoice_item
+         WHERE order_item_id IN (SELECT item_id FROM sales_order_item
+         WHERE order_id = ?)
         "
       )
+      query.execute invoice_order_id
     end
+
+    # varchar attribute for product
+    def self.eav_attr_for product_id, attr_id
+      query = client.prepare
+        "
+         SELECT * FROM catalog_product_entity_varchar
+         WHERE attribute_id = ?
+         AND entity_id = ?
+        "
+      )
+      query.execute attr_id, product_id
+    end
+
   end
 end
